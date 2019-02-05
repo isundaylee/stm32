875406f6d5fed55c3165af543dfeecaf7ef098d7
Jiahao Li <reg@ljh.me>
2019-02-05 21:37:06 +0800

    Adds timer_blinky example

---
+++ app/stm32f446/examples/timer_blinky/main.cpp
@@ -0,0 +1,30 @@
+#include <Utils.h>
+
+#include <GPIO.h>
+#include <Timer.h>
+
+void timerHandler() {
+  static bool value = 0;
+
+  value = !value;
+
+  if (value) {
+    GPIO_A.set(1);
+  } else {
+    GPIO_A.clear(1);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Main!
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" void main() {
+  GPIO_A.enable();
+  GPIO_A.setMode(1, GPIO::PinMode::OUTPUT);
+
+  Timer_2.enable(1000, 8000, timerHandler);
+
+  while (true) {
+  }
+}
