0d8741212fc7f7ac2e044396eca9c061445e61f8
Jiahao Li <reg@ljh.me>
2019-02-09 22:10:43 +0800

    Link is up in spi_enc28j60 example!

---
+++ app/stm32f446/examples/spi_enc28j60/ENC28J60.h
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <stdint.h>
+
+enum class ControlRegBank {
+  BANK_0,
+  BANK_1,
+  BANK_2,
+  BANK_3,
+};
+
+enum class ControlRegAddress {
+  ERDPTL = 0x00,
+  ERDPTH,
+  EWRPTL,
+  EWRPTH,
+  ETXSTL,
+  ETXSTH,
+  ETXNDL,
+  ETXNDH,
+  ERXSTL,
+  ERXSTH,
+  ERXNDL,
+  ERXNDH,
+  ERXRDPTL,
+  ERXRDPTH,
+  ERXWRPTL,
+  ERXWRPTH,
+  EDMASTL,
+  EDMASTH,
+  EDMANDL,
+  EDMANDH,
+  EDMADSTL,
+  EDMADSTH,
+  EDMACSL,
+  EDMACSH,
+  // --
+  // --
+  // Reserved
+  EIE = 0x1B,
+  EIR,
+  ESTAT,
+  ECON2,
+  ECON1,
+
+  EHT0 = 0x00,
+  EHT1,
+  EHT2,
+  EHT3,
+  EHT4,
+  EHT5,
+  EHT6,
+  EHT7,
+  EPMM0,
+  EPMM1,
+  EPMM2,
+  EPMM3,
+  EPMM4,
+  EPMM5,
+  EPMM6,
+  EPMM7,
+  EPMCSL,
+  EPMCSH,
+  // --
+  // --
+  EPMOL = 0x14,
+  EPMOH,
+  // Reserved
+  // Reserved
+  ERXFCON = 0x18,
+  EPKTCNT,
+
+  MACON1 = 0x00,
+  // Reserved
+  MACON3 = 0x02,
+  MACON4,
+  MABBIPG,
+  // --
+  MAIPGL = 0x06,
+  MAIPGH,
+  MACLCON1,
+  MACLCON2,
+  MAMXFLL,
+  MAMXFLH,
+  // Reserved
+  // Reserved
+  // Reserved
+  // --
+  // Reserved
+  // Reserved
+  MICMD = 0x12,
+  // --
+  MIREGADR = 0x14,
+  // Reserved
+  MIWRL = 0x16,
+  MIWRH,
+  MIRDL,
+  MIRDH,
+
+  MAADR5 = 0x00,
+  MAADR6,
+  MAADR3,
+  MAADR4,
+  MAADR1,
+  MAADR2,
+  EBSTSD,
+  EBSTCON,
+  EBSTCSL,
+  EBSTCSH,
+  MISTAT,
+  // --
+  // --
+  // --
+  // --
+  // --
+  // --
+  // --
+  EREVID = 0x12,
+  // --
+  // --
+  ECOCON = 0x15,
+  // Reserved
+  EFLOCON = 0x17,
+  EPAUSL,
+  EPAUSH
+};
+
+enum class PHYRegAddress {
+  PHCON1 = 0x00,
+  PHSTAT1 = 0x01,
+  PHID1 = 0x02,
+  PHID2 = 0x03,
+  PHCON2 = 0x10,
+  PHSTAT2 = 0x11,
+  PHIE = 0x12,
+  PHIR = 0x13,
+  PHLCON = 0x14,
+};
+
+enum class Opcode {
+  READ_CONTROL_REGISTER = 0b000,
+  READ_BUFFER_MEMORY = 0b001,
+  WRITE_CONTROL_REGISTER = 0b010,
+  WRITE_BUFFER_MEMORY = 0b011,
+  BIT_FIELD_SET = 0b100,
+  BIT_FIELD_CLEAR = 0b101,
+  SYSTEM_RESET_COMMAND = 0b111,
+};
+
+const uint8_t MACON1_TXPAUS = 0b00001000;
+const uint8_t MACON1_RXPAUS = 0b00000100;
+const uint8_t MACON1_PASSALL = 0b00000010;
+const uint8_t MACON1_MARXEN = 0b00000001;
+
+const uint8_t MACON3_PADCFG2 = 0b10000000;
+const uint8_t MACON3_PADCFG1 = 0b01000000;
+const uint8_t MACON3_PADCFG0 = 0b00100000;
+const uint8_t MACON3_TXCRCEN = 0b00010000;
+const uint8_t MACON3_PHDREN = 0b00001000;
+const uint8_t MACON3_HFRMEN = 0b00000100;
+const uint8_t MACON3_FRMLNEN = 0b00000010;
+const uint8_t MACON3_FULDPX = 0b00000001;
+
+const uint8_t MACON4_DEFER = 0b01000000;
+const uint8_t MACON4_BPEN = 0b00100000;
+const uint8_t MACON4_NOBKOFF = 0b00010000;
+
+const uint16_t PHCON1_PRST = 0b1000000000000000;
+const uint16_t PHCON1_PLOOPBK = 0b0100000000000000;
+const uint16_t PHCON1_PPWRSV = 0b0000100000000000;
+const uint16_t PHCON1_PDPXMD = 0b0000000100000000;
+
+const uint16_t PHSTAT1_PFDPX = 0b0001000000000000;
+const uint16_t PHSTAT1_PHDPX = 0b0000100000000000;
+const uint16_t PHSTAT1_LLSTAT = 0b0000000000000100;
+const uint16_t PHSTAT1_JBSTAT = 0b0000000000000010;
---
+++ app/stm32f446/examples/spi_enc28j60/main.cpp
@@ -0,0 +1,204 @@
+#include <Utils.h>
+
+#include <GPIO.h>
+#include <SPI.h>
+#include <USART.h>
+
+#include "ENC28J60.h"
+
+uint8_t generateHeaderByte(Opcode opcode, ControlRegAddress addr) {
+  return (static_cast<uint8_t>(opcode) << 5) + static_cast<uint8_t>(addr);
+}
+
+void selectControlRegBank(ControlRegBank bank) {
+  uint16_t data[] = {
+      generateHeaderByte(Opcode::BIT_FIELD_CLEAR, ControlRegAddress::ECON1),
+      0b00000011};
+
+  GPIO_B.clear(12);
+  SPI_2.transact(data, sizeof(data) / sizeof(data[0]));
+  GPIO_B.set(12);
+
+  data[0] = generateHeaderByte(Opcode::BIT_FIELD_SET, ControlRegAddress::ECON1);
+  data[1] = static_cast<uint8_t>(bank);
+
+  GPIO_B.clear(12);
+  SPI_2.transact(data, sizeof(data) / sizeof(data[0]));
+  GPIO_B.set(12);
+}
+
+uint8_t readETHReg(ControlRegBank bank, ControlRegAddress addr) {
+  selectControlRegBank(bank);
+
+  uint16_t data[] = {generateHeaderByte(Opcode::READ_CONTROL_REGISTER, addr),
+                     0x00};
+
+  GPIO_B.clear(12);
+  SPI_2.transact(data, sizeof(data) / sizeof(data[0]));
+  GPIO_B.set(12);
+
+  return static_cast<uint8_t>(data[1]);
+}
+
+uint8_t writeControlReg(ControlRegBank bank, ControlRegAddress addr,
+                        uint8_t value) {
+  selectControlRegBank(bank);
+
+  uint16_t data[] = {generateHeaderByte(Opcode::WRITE_CONTROL_REGISTER, addr),
+                     value};
+
+  GPIO_B.clear(12);
+  SPI_2.transact(data, sizeof(data) / sizeof(data[0]));
+  GPIO_B.set(12);
+
+  return static_cast<uint8_t>(data[1]);
+}
+
+uint8_t readMACMIIReg(ControlRegBank bank, ControlRegAddress addr) {
+  selectControlRegBank(bank);
+
+  uint16_t data[] = {generateHeaderByte(Opcode::READ_CONTROL_REGISTER, addr),
+                     0x00, 0x00};
+
+  GPIO_B.clear(12);
+  SPI_2.transact(data, sizeof(data) / sizeof(data[0]));
+  GPIO_B.set(12);
+
+  return static_cast<uint8_t>(data[2]);
+}
+
+static void initializeETH() {
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXSTL, 0x00);
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXSTH, 0x00);
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXNDL, 0xFF);
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXNDH, 0x0F);
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXRDPTL, 0x00);
+  writeControlReg(ControlRegBank::BANK_0, ControlRegAddress::ERXRDPTH, 0x00);
+
+  writeControlReg(ControlRegBank::BANK_1, ControlRegAddress::ERXFCON, 0x00);
+
+  WAIT_UNTIL(
+      BIT_IS_SET(readETHReg(ControlRegBank::BANK_0, ControlRegAddress::ESTAT),
+                 0b00000001));
+}
+
+static void initializeMAC() {
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MACON1,
+                  MACON1_MARXEN | MACON1_TXPAUS | MACON1_RXPAUS);
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MACON3,
+                  MACON3_PADCFG0 | MACON3_FRMLNEN | MACON3_FULDPX);
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MACON4,
+                  MACON4_DEFER);
+
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MAMXFLL, 0x00);
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MAMXFLH, 0x06);
+
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MABBIPG, 0x15);
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MAIPGL, 0x12);
+
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR1, 0x11);
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR2, 0x22);
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR3, 0x33);
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR4, 0x44);
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR5, 0x55);
+  writeControlReg(ControlRegBank::BANK_3, ControlRegAddress::MAADR6, 0x66);
+}
+
+uint16_t readPHYReg(PHYRegAddress addr) {
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MIREGADR,
+                  static_cast<uint8_t>(addr));
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MICMD, 0x01);
+  WAIT_UNTIL(BIT_IS_CLEAR(
+      readMACMIIReg(ControlRegBank::BANK_3, ControlRegAddress::MISTAT), 0x01));
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MICMD, 0x00);
+
+  uint8_t low = readMACMIIReg(ControlRegBank::BANK_2, ControlRegAddress::MIRDL);
+  uint8_t high =
+      readMACMIIReg(ControlRegBank::BANK_2, ControlRegAddress::MIRDH);
+
+  return static_cast<uint16_t>(low) + (static_cast<uint16_t>(high) << 8);
+}
+
+void writePHYReg(PHYRegAddress addr, uint16_t value) {
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MIREGADR,
+                  static_cast<uint8_t>(addr));
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MIWRL,
+                  static_cast<uint8_t>(value & 0x00FF));
+  writeControlReg(ControlRegBank::BANK_2, ControlRegAddress::MIWRH,
+                  static_cast<uint8_t>(value >> 8));
+  WAIT_UNTIL(BIT_IS_CLEAR(
+      readMACMIIReg(ControlRegBank::BANK_3, ControlRegAddress::MISTAT), 0x01));
+}
+
+void printETHReg(ControlRegBank bank, ControlRegAddress addr,
+                 char const* name) {
+  USART_1.write(name);
+  USART_1.write(": ");
+  USART_1.write(HexString(readETHReg(bank, addr)));
+  USART_1.write("\r\n");
+}
+
+void printMACMIIReg(ControlRegBank bank, ControlRegAddress addr,
+                    char const* name) {
+  USART_1.write(name);
+  USART_1.write(": ");
+  USART_1.write(HexString(readMACMIIReg(bank, addr)));
+  USART_1.write("\r\n");
+}
+
+void printPHYReg(PHYRegAddress addr, char const* name) {
+  USART_1.write(name);
+  USART_1.write(": ");
+  USART_1.write(HexString(readPHYReg(addr)));
+  USART_1.write("\r\n");
+}
+
+static void initializePHY() {
+  writePHYReg(PHYRegAddress::PHCON1, PHCON1_PDPXMD);
+}
+
+static void initialize() {
+  initializeETH();
+  initializeMAC();
+  initializePHY();
+
+  USART_1.write("Waiting for link");
+  while (BIT_IS_CLEAR(readPHYReg(PHYRegAddress::PHSTAT1), PHSTAT1_LLSTAT)) {
+    USART_1.write(".");
+    DELAY(1000000);
+  }
+  USART_1.write("\r\n");
+  USART_1.write("Link is up!\r\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Main!
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" void main() {
+  // Sets up USART 1
+  GPIO_A.enable();
+  GPIO_A.setMode(9, GPIO::PinMode::ALTERNATE, 7);  // TX
+  GPIO_A.setMode(10, GPIO::PinMode::ALTERNATE, 7); // RX
+  USART_1.enable(115200);
+
+  // Sets up SPI 2
+  GPIO_B.enable();
+  GPIO_B.setMode(12, GPIO::PinMode::OUTPUT); // NSS / LOAD
+  GPIO_B.set(12);
+  GPIO_B.setMode(13, GPIO::PinMode::ALTERNATE, 5); // SCK
+  GPIO_B.setMode(14, GPIO::PinMode::ALTERNATE, 5); // MISO
+  GPIO_B.setMode(15, GPIO::PinMode::ALTERNATE, 5); // MOSI
+  SPI_2.configureMaster(SPI::ClockPolarity::IDLE_LOW,
+                        SPI::ClockPhase::SAMPLE_ON_FIRST_EDGE,
+                        SPI::DataFrameFormat::BYTE, SPI::BaudRate::PCLK_OVER_4,
+                        SPI::NSSMode::MANUAL);
+  SPI_2.enable();
+
+  USART_1.write("Hello, SPI ENC28J60!\r\n");
+
+  initialize();
+
+  while (true) {
+  }
+}
