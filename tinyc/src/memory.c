ad6de03fde7b9620de14a844c1185d2ed8c14f29
Jiahao Li <reg@ljh.me>
2019-02-26 02:13:18 -0500

    Makes `malloc` (very slightly) better

--- tinyc/src/memory.c
+++ tinyc/src/memory.c
@@ -9,20 +9,57 @@ static uint8_t __attribute__((aligned(4))) heap[HEAP_SIZE];
 static uint8_t* heap_cur = heap;
 static uint8_t* heap_top = heap + HEAP_SIZE;
 
+typedef struct _node_t node_t;
+
+struct _node_t {
+  size_t size;
+
+  union {
+    struct {
+      node_t* next;
+    } free;
+
+    struct {
+    } allocated;
+  } metadata;
+};
+
+node_t* freelist_head = NULL;
+
 void* malloc(size_t size) {
+  size += sizeof(node_t);
+  size += (ALIGNMENT - (size % ALIGNMENT));
+
+  // Try to allocate from the freelist first.
+  for (node_t** cur = &freelist_head; *cur != NULL;
+       cur = &(*cur)->metadata.free.next) {
+    if ((*cur)->size >= size) {
+      void* allocated = (void*)((uintptr_t)*cur + sizeof(node_t));
+      *cur = (*cur)->metadata.free.next;
+      return allocated;
+    }
+  }
+
   if (heap_cur + size > heap_top) {
     return NULL;
   }
 
-  size += (ALIGNMENT - (size % ALIGNMENT));
+  node_t* node = (node_t*)heap_cur;
+  node->size = size;
 
-  void* allocated = (void*)heap_cur;
   heap_cur += size;
 
-  return allocated;
+  return (void*)((uintptr_t)node + sizeof(node_t));
 }
 
-void free(void* ptr) { (void)ptr; }
+void free(void* ptr) {
+  node_t* node = (node_t*)((uintptr_t)ptr - sizeof(node_t));
+
+  node->metadata.free.next = freelist_head;
+  freelist_head = node;
+
+  (void)ptr;
+}
 
 int memcmp(const void* a, const void* b, size_t n) {
   uint8_t* ua = (uint8_t*)a;
